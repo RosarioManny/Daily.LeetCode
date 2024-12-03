2ad8105f8db79f3e77dd7ccc76ca995107171366
RosarioManny <rosario.emm47@gmail.com>
2024-12-03 16:43:57 -0500

    Attempt 1

--- December_2024/notes.js
+++ December_2024/notes.js
@@ -144,7 +144,29 @@ const fizzBuzz = function(n) {
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 4. LEETCODE #1342. Number of Steps to Reduce a Number to Zero ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 // Given an integer num, return the number of steps to reduce it to zero.
 // In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
-const numberOfSteps = () => {
-  
+
+// CONCEPTUALIZE 
+// Step 1) 14 is even; divide by 2 and obtain 7. 
+// Step 2) 7 is odd; subtract 1 and obtain 6.
+// Step 3) 6 is even; divide by 2 and obtain 3. 
+// Step 4) 3 is odd; subtract 1 and obtain 2. 
+// Step 5) 2 is even; divide by 2 and obtain 1. 
+// Step 6) 1 is odd; subtract 1 and obtain 0.
+
+const num = 14
+
+const numberOfSteps = (num) => {
+  while (num > 0) {
+    if (num === 0) {
+      return num
+    } else if (num % 2 === 0) {
+      num / 2
+    } else {
+      num - 1
+    }
+    return num
+  } 
 }
+
+console.log(numberOfSteps())
 
