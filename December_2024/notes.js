db6c6b2a5102a7b8c48cc43bb63c392a467cdca4
RosarioManny <rosario.emm47@gmail.com>
2024-12-04 17:57:34 -0500

    Ransom Note

--- December_2024/notes.js
+++ December_2024/notes.js
@@ -201,3 +201,18 @@ const middleNode = (head) => {
 // At this point, middle will be at the middle of the list.
 // TIME COMPLEXITY = O(n) 
 // SPACE COMPLEXITY = O(1) 
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 6. LEETCODE #383. Ransom Note ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+// Each letter in magazine can only be used once in ransomNote.
+// Example 2:
+
+// Input: ransomNote = "aa", magazine = "ab"
+// Output: false
+// Example 3:
+
+// Input: ransomNote = "aa", magazine = "aab"
+// Output: true
+
+const canConstruct = (ransomNote, magazine) => {
+    
+};
