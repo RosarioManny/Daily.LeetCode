048b2d1f537e7d5ddbe47cf15ace87aeb401b709
RosarioManny <rosario.emm47@gmail.com>
2024-12-06 16:44:52 -0500

    Find Numbers with Even Number of Digits

--- December_2024/notes.js
+++ December_2024/notes.js
@@ -282,3 +282,14 @@ console.log(findMaxConsecutiveOnes())
 // Storing the length everytime a 1 is saved instead of just when it reaches its max.
 
 // Scrapped the array idea and found out I can just use counters. 
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 7. LEETCODE Find Numbers with Even Number of Digits ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+// Given an array nums of integers, return how many of them contain an even number of digits.
+// Input: nums = [12,345,2,6,7896]
+// Output: 2
+// Explanation: 
+// 12 contains 2 digits (even number of digits). 
+// 345 contains 3 digits (odd number of digits). 
+// 2 contains 1 digit (odd number of digits). 
+// 6 contains 1 digit (odd number of digits). 
+// 7896 contains 4 digits (even number of digits). 
+// Therefore only 12 and 7896 contain an even number of digits.
