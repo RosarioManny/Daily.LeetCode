718a1729fe4928eb2a7836d1c88cf40e71320e4e
RosarioManny <rosario.emm47@gmail.com>
2024-12-10 21:07:19 -0500

    Setup #12

--- December_2024/notes.js
+++ December_2024/notes.js
@@ -431,4 +431,15 @@ console.log(mergeArray())
 // 6. Loop through nums1 and check if the nums1[i] = 0. If so push the nums2[counter], starting at 0. Then increase that counter. 
 // 7. Sort nums1 array to non-descending(ascending) order.
 // 8. Return nums1
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 12. LEETCODE #121. Best Time to Buy and Sell Stock ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+// Example 1:
 
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+//Psuedo-Code
