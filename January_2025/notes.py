5ba8a16eb2d1d707d816ef777710e57f4a2356a0
RosarioManny <rosario.emm47@gmail.com>
2025-01-31 19:57:29 -0500

    Valid Parentheses Attemps 3

--- January_2025/notes.py
+++ January_2025/notes.py
@@ -165,4 +165,43 @@ def heighChecker():
       counter += 1
   return counter
 
-print(heighChecker())
+# print(heighChecker())
+
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 6. LEETCODE 20. Valid Parentheses ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+
+# s = "()[]{}" # true
+# s = "(]" # false
+s = "}[" # false
+
+stack = []
+map = {
+    '(': ')',
+    '{': '}',
+    '[': ']'
+}
+def checkBrackets(brace):
+  if len(brace) % 2 != 0:
+    return False
+  # print('S :: ', s)
+  for i in range(len(brace)):
+    if brace[i] in map:
+      stack.append(brace[i])
+    else:
+      if len(stack) == 0:
+        return False
+      if (map.get(stack.pop()) != brace[i]):
+        return False
+    # print('Stack :: ' , stack)
+  # print("out of loop Stack :: ", stack)
+  if len(stack) == 0:
+    return True
+  else:
+    return False
+  
+print(checkBrackets(s))
