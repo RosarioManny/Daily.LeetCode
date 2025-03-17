37f6b6e3fcf23fa95ef955677cf72587e06b5941
RosarioManny <rosario.emm47@gmail.com>
2025-03-17 17:09:58 -0400

    Merged Sorted List Atmpt2

--- March_2025/notes.py
+++ March_2025/notes.py
@@ -14,38 +14,32 @@
 # Input: list1 = [], list2 = [0]
 # Output: [0]
 
-x = [1, 3, 4]
+x = []
 y = [2, 4, 6]
 
 def mergeTwoList(list1, list2):
   sortedList = []
   p1, p2 = 0, 0
-  head = list1
-  # Check if list 1 is empty
-  if not head:
-    head = list2
-  
-  while(p1 < len(list1) and p2 < len(list2)):
-    if list1[p1] < list2[p2] and p1 != len(head):
+  # Loop through the (longest) list
+  while p1 < len(list1) and p2 < len(list2):
+    if list1[p1] < list2[p2]:
       sortedList.append(list1[p1])
       p1 += 1
-    elif list1[p1] > list2[p2] and p1 != len(head):
-      sortedList.append(list2[p2])
-      p2 += 1
     else:
-      sortedList.append(list1[p1])
       sortedList.append(list2[p2])
-      p1 += 1
       p2 += 1
-    # print("sortedL >>", sortedList)
+  
+  # Append remaining elements in list1
+  while p1 < len(list1):
+    sortedList.append(list1[p1])
+    p1 += 1
+  
+  # Append remaining elements in list2
+  while p2 < len(list2):
+    sortedList.append(list2[p2])
+    p2 += 1
     
   
-  for i in range(p2, len(list2)):
-    print("l2 >> ", list2[i])
-    sortedList.append(list2[i])
-    
-  #   print(p1, p2)
-  # print("SL >> ", sortedList)
   return sortedList
 
 print("Answer >> ", mergeTwoList(x, y))
